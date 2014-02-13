44945b6c9ed6a43fd6a476b1ac15a54176d3af86
yapadubpadoo <yapadubpadoo@gmail.com>
2014-02-13 12:33:34 +0700

    Refactor testUpdateOneRow

--- AccessLogAPI.php
+++ AccessLogAPI.php
@@ -5,49 +5,21 @@ class AccessLogAPI {
 		$this->db = $db;
 	}
 	
-	function deleteById($id) {
-		$db = $this->db;
-		$stmt = $db->prepare("DELETE FROM api_log WHERE id = ?");
-		$stmt->execute(array($id));
-		return $stmt->rowCount();
-	}
 	
-	function insert($value_array)
-	{
-		$sql = "INSERT INTO api_log(ip, service_name, `datetime`, response_time,response_massage) VALUES (:ip,:service_name,:datetime,:response_time,:response_massage)";
-		$q = $this->db->prepare($sql);
+	function updateById($id, $keys, $values){
+			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$update = "";
+			foreach($keys as $key) {
+				if($update!="") $update .= ", ";
+				$update .= $key."=?";
+			}
+			$stmt = $this->db->prepare("UPDATE api_log SET ".$update." where id=?");
+			$params = array_merge($values, array($id));
 
-		return $q->execute(
-			[
-				$value_array['ip']=>$ip,
-				$value_array['service_name']=>$service_name,
-				$value_array['datetime']=>$datetime,
-				$value_array['response_time']=>$response_time,
-				$value_array['response_massage']=>$response_massage
-			]
-	    );
+			$stmt->execute($params);
 
-	}
-	
-	function updateById($id, $sql){
-		//	try {
-			//$dbh = new PDO('mysql:host=localhost;dbname='.$database, $user, $pass);
-			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		//try {
-			$stmt = $this->db->prepare("UPDATE ".$table." SET ip=:ip where id=1");
-			//$now = date("Y-m-d H:m:i");
-			//$stmt->bindParam(':now', $now);
-			$ip = '158.168.2.7';
-					$stmt->bindParam(':ip', $ip);
-	//$stmt->bindParam(':value', $value);
-			$dbh->beginTransaction();
-			//$dbh->exec("insert into ".$table." (ip, service_name, datetime, response_time,response_message) values('192.168.1.1', 'mysqld', '".date("Y-m-d H:m:s")."', '".date("Y-m-d H:m:s")."', 'OK')");
-		//$dbh->exec("insert into salarychange (id, amount, changedate) values (23, 50000, NOW())");
-			$stmt->execute();
-			$dbh->commit();
-		//} catch (Exception $e){
-		//	echo $e->getMessage();
-		//}	
+			return  $stmt->rowCount();
+
 	}
 }
 ?>
--- AccessLogAPITest.php
+++ AccessLogAPITest.php
@@ -1,6 +1,36 @@
 <?php
 	require_once 'AccessLogAPI.php';
-	class AccessLogAPITest extends PHPUnit_Framework_Testcase {
-		
+	require_once 'MockPDO.php';
+	class AccessLogAPITest extends PHPUnit_Framework_TestCase {
+		function testUpdate1Row() {
+			$expected = 1;
+			
+			$stubPDOStmt = $this->getMock('PDOStatement');
+			$stubPDOStmt->expects($this->once())
+				->method('execute')
+				->will($this->returnValue(true));
+			$stubPDOStmt->expects($this->once())
+				->method('rowCount')
+				->will($this->returnValue(1));
+			
+			
+			$stubPDO = $this->getMock('MockPDO');
+			$stubPDO->expects($this->once())
+				->method('setAttribute');
+			$stubPDO->expects($this->once())
+				->method('prepare')
+				->will($this->returnValue($stubPDOStmt));
+			
+			$accessLogAPI = new AccessLogAPI();	
+			$accessLogAPI->setPDO($stubPDO);
+			
+			$id = 1;
+			$keys = array("service_name");
+			$values = array("AccessLogAPI");
+			$result = $accessLogAPI->updateById($id, $keys, $values);
+			
+			
+			$this->assertEquals($expected, $result);
+		}
 	}
 ?>
